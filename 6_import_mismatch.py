571e4d8bb61cfe8231de38fe2ff2c7758d59492f
Saianiruthm <116950213+Saianiruthm@users.noreply.github.com>
2024-02-21 00:54:35 +0530

    Update 6_import_mismatch.py

--- 6_import_mismatch.py
+++ 6_import_mismatch.py
@@ -5,30 +5,30 @@ import os
 
 # Define the Label Studio API keys and URLs for both annotators
 API_KEY_ANNOTATOR_1 = 'API_1'
-API_KEY_ANNOTATOR_2 = 'API_2'
+#API_KEY_ANNOTATOR_2 = 'API_2'
 LABEL_STUDIO_URL = 'http://localhost:8080/'
 
 # Initialize the Label Studio clients for both annotators
 client_1 = Client(url=LABEL_STUDIO_URL, api_key=API_KEY_ANNOTATOR_1)
-client_2 = Client(url=LABEL_STUDIO_URL, api_key=API_KEY_ANNOTATOR_2)
+#client_2 = Client(url=LABEL_STUDIO_URL, api_key=API_KEY_ANNOTATOR_2)
 
 #Get project from project id
 with open('ids.txt', 'r') as f:
     lines = f.readlines()
 project_1_id = int(lines[0].strip())
-project_2_id = int(lines[1].strip())
+#project_2_id = int(lines[1].strip())
 
 project_1 = Project.get_from_id(client_1, project_1_id)
-project_2 = Project.get_from_id(client_2, project_2_id)
+#project_2 = Project.get_from_id(client_2, project_2_id)
 
 project_1.delete_all_tasks()
-project_2.delete_all_tasks()
+#project_2.delete_all_tasks()
 
 json_file_path = r"E:\task_2\json_mismatch_file"
 
 #Import Tasks
 import_tasks_from_json(project_1, json_file_path)
-import_tasks_from_json(project_2, json_file_path)
+#import_tasks_from_json(project_2, json_file_path)
 
 
 
